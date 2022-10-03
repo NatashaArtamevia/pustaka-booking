4166cddae1c913d20ca22f8e111624e78bcf4d6c
NatashaArtamevia <113346062+NatashaArtamevia@users.noreply.github.com>
2022-10-03 14:43:07 +0700

    Add files via upload

---
+++ application/views/v_about.php
@@ -0,0 +1,25 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4><Nama></h4>
+    <ul type="disc">
+        <li>Nama Depan : Natasha</li>
+        <li>Nama Belakang : Artamevia</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li>Perum Kedungwaringin</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Bekasi</li>
+    </ul>
+
+    <h4>Olahraga Favorit</h4>
+    <ul type="square">
+        <li>Tenis Meja</li>
+        <li>Badminton</li>
+    </ul>
+</section>
+
